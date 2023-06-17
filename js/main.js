dbff31692169e0feded062e673c6c44193445a59
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2023-06-17 14:23:02 +0200

    changes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const mobileNavToggle = document.querySelector('.mobile-nav-toggle__button');
+const mainSiteNav = document.querySelector('.main-nav');
+const orderDate = document.querySelector('.order-summary__date');
+
+mobileNavToggle.addEventListener('click', () => {
+  mainSiteNav.classList.toggle('is-open');
+  mobileNavToggle.classList.toggle('is-open');
+})
+
+if (orderDate) {
+  orderDate.append(new Date().toLocaleDateString());
+}
